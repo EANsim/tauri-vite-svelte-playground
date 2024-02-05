77ad64ee50d984dffca1e726007ae4e09131c233
Gesard <quick.answer.is.good@gmai.com>
2024-02-05 12:29:57 +0300

    tested select in sqlite.

--- src-tauri/src/server/test_db.rs
+++ src-tauri/src/server/test_db.rs
@@ -1,44 +1,67 @@
 use rusqlite::{Connection, Result};
+use std::path::Path;
 
 #[derive(Debug)]
-struct Person {
+struct Knop {
     id: i32,
+    block: String,
+    types: String,
     name: String,
-    data: Option<Vec<u8>>,
+    state: i32,
+    max_state: i32,
+    comment: String,
 }
 
 fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+    let path = Path::new("states.sqlite");
+    let conn = Connection::open(path)?;
 
+/*
     conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
-            data  BLOB
-        )",
+        "
+CREATE TABLE person (
+	id INTEGER PRIMARY KEY,
+	block TEXT NOT NULL,
+	types TEXT NOT NULL,
+	name TEXT NOT NULL,
+	state INTEGER,
+	max_state INTEGER,
+    comment TEXT NOT NULL
+)
+        ",
         (), // empty list of parameters.
     )?;
-    let me = Person {
+ */
+    let me = Knop {
         id: 0,
-        name: "Steven".to_string(),
-        data: None,
+        block: "test".to_string(),
+        types: "лампа".to_string(),
+        name: "q1".to_string(),
+        state: 0,
+        max_state: 2,
+        comment: "test".to_string(),
     };
+    /*
     conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
+        "INSERT INTO person (block, types, name) VALUES (?1, ?2, ?3)",
+        (&me.block, &me.types, &me.name),
+    )?;*/
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let mut stmt = conn.prepare("SELECT id, block, types, name, state, max_state, comment FROM person")?;
     let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
+        Ok(Knop {
             id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
+            block: row.get(1)?,
+            types: row.get(2)?,
+            name: row.get(3)?,
+            state: row.get(4)?,
+            max_state: row.get(5)?,
+            comment: row.get(6)?,
         })
     })?;
 
     for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
+        println!("Found {:?}", person.unwrap());
     }
     Ok(())
 }
