34416deb7a497d60465e3e26aa73bd7076ad4e29
eugene.gleba <glebajk@gmail.com>
2024-02-09 22:39:53 +0300

    Fixed formatting styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
-  const exit_application = () => {
-    close();
-  };
+    const exit_application = () => {
+        close();
+    };
 </script>
 
 <main>
-  <ul class="button-list">
-    <li><button>Создать сервер</button></li>
-    <li><button>Подключиться к серверу</button></li>
-    <li><button>Настройки</button></li>
-    <li><button on:click={exit_application}>Выход</button></li>
-  </ul>
+    <ul class="button-list">
+        <li><button>Создать сервер</button></li>
+        <li><button>Подключиться к серверу</button></li>
+        <li><button>Настройки</button></li>
+        <li><button on:click={exit_application}>Выход</button></li>
+    </ul>
 </main>
 
 <style>
-  :global(body) {
-    background-image: url("./assets/main_menu_background.jpg");
-    background-size: cover;
-    background-position: center;
-    min-height: 100vh;
-    min-width: 100vw;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
+    :global(body) {
+        background-image: url('./assets/main_menu_background.jpg');
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        min-width: 100vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
 
-  .button-list {
-    list-style: none;
-    margin: 100px 0 20px;
-    padding: 50px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+    .button-list {
+        list-style: none;
+        margin: 100px 0 20px;
+        padding: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
-  li {
-    margin-bottom: 10px;
-  }
+    li {
+        margin-bottom: 10px;
+    }
 
-  button {
-    padding: 10px;
-    width: 250px;
-    font-size: 16px;
-    background-color: rgba(22, 21, 21, 0.9);
-    cursor: pointer;
-  }
+    button {
+        padding: 10px;
+        width: 250px;
+        font-size: 16px;
+        background-color: rgba(22, 21, 21, 0.9);
+        cursor: pointer;
+    }
 </style>
