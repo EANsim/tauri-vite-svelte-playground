4230e57afd903bf0d56a464203fbb3f515c2bf83
eugene.gleba <glebajk@gmail.com>
2024-02-10 13:05:35 +0300

    Small renamings to .svelte

---
+++ src/lib/test_counter_lib.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
+</script>
+
+<button on:click={increment}>
+    count is {count}
+</button>
+
+<!--
+Usage of the library example:
+<script lang="ts">
+    import test_counter from './lib/test_counter_lib.svelte'
+</script>
+
+<main>
+    <div class="card">
+        <test_counter />
+    </div>
+</main>
+-->
